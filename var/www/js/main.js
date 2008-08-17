35f7cad3fc31d4c77276f2b6c3dd3fa80bfbdc39
kouhei <ueno@nyaxtstep.com>
2008-08-17 15:03:53 +0900

    score based highlighting

--- var/www/js/main.js
+++ var/www/js/main.js
@@ -9,17 +9,41 @@ $(function() {
   var latestStatus = new Date(); latestStatus.setTime(latestStatus.getTime() - 86400 * 1000); // tweets from yesterday
   var columnAB = 'a';
 
+  var regAtReply = /@\w+/gi;
+  // adopted from http://furyu.tea-nifty.com/script/cvtUrlToLink.user.js
+  var regURL = /(h?)(ttps?:\/\/[-_.!~*()a-zA-Z0-9;/?:@&=+$,%#]+)/mgi;
+
   function appendStatus()
   {
     var scrname = this['user']['screen_name'];
+  
+    var scoreclass = '';
+    if(this['score'] > 500)
+    {
+      scoreclass = 'veryhighscore highscore';
+    }
+    else if(this['score'] > 400)
+    {
+      scoreclass = 'highscore'; 
+    }
+    else if(this['score'] < 130)
+    {
+      scoreclass = 'lowscore';
+    }
+
+    var text = this['text'];
+    text = text
+      .replace(regURL, function(url) { return '<a href="'+url+'" target="_blank">'+url+'</a>'; })
+      .replace(regAtReply, function(nick) { return '<a href="http://twitter.com/'+nick.substr(1,100)+'" target="_blank">'+nick+'</a>'; });
+
     status = statusTemplate
-      .replace(/%ab%/, columnAB)
+      .replace(/%ab%/, columnAB + ' ' + scoreclass)
       .replace(/%img%/, this['user']['profile_image_url'])
       .replace(/%nick%/, "<a href='http://twitter.com/"+scrname+"'>"+scrname+"</a>")
       .replace(/%username%/, this['user']['name'])
       .replace(/%score%/, this['score'])
       .replace(/%created_at%/, this['created_at'])
-      .replace(/%message%/, this['text']);
+      .replace(/%message%/, text);
 
     $("#statuses_container").prepend(status);
     if(columnAB == 'a')
